ea9d1ceb9c149983711680c2eebc53dbfbfba8db
krisi0505 <krisi0505@abv.bg>
2017-04-23 15:00:23 +0300

    create board

--- board.js
+++ board.js
@@ -8,7 +8,7 @@ class Board{
 		return this._rows;
 	}
 
-	set rows(){
+	set rows(value){
 		this._rows = value;
 	}
 
@@ -16,9 +16,40 @@ class Board{
 		return this._cols;
 	}
 
-	set cols(){
+	set cols(value){
 		this._cols = value;
+	}
+
+	createBoard(){
+		var table = document.getElementById('board');
+
+		var fragment = document.createDocumentFragment();
+
+		var tr = document.createElement('TR');
+		tr.className = 'row';
+
+        var td = document.createElement('TD');
+        td.className = 'col';
+
+        var button = document.createElement('button');
+        button.className = 'field';
+        td.appendChild(button);
+
+        for (var i = 0; i < this._cols; i += 1) {
+            var col = td.cloneNode(true);
+        	tr.appendChild(col);
+        }
+
+        for (var i = 0; i < this._rows; i += 1) {
+             var row = tr.cloneNode(true);
+             fragment.appendChild(row);
+        }
+
+        table.appendChild(fragment);
 	}
 
 	//TODO add methods, fields and properties
 }
+
+var board = new Board(16, 30);
+board.createBoard();
