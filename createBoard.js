11066142c616527d87a874e141aea89ccd5ba079
krisi0505 <krisi0505@abv.bg>
2017-04-23 13:02:58 +0300

    add createBoard.js

---
+++ createBoard.js
@@ -0,0 +1,9 @@
+var newGameButtonElement = document.getElementById("new-game");
+newGameButtonElement.addEventListener('click', createTable);
+
+function createTable(){
+	var boardDivElement = document.getElementById("board");
+
+	var tableElement = document.createElement('table');
+	
+}
