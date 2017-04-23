70725ef8ab1265f5a2052721b171c04e30aaf96b
krisi0505 <krisi0505@abv.bg>
2017-04-23 13:24:31 +0300

    delete createTable.js

--- createBoard.js
+++
@@ -1,9 +0,0 @@
-var newGameButtonElement = document.getElementById("new-game");
-newGameButtonElement.addEventListener('click', createTable);
-
-function createTable(){
-	var boardDivElement = document.getElementById("board");
-
-	var tableElement = document.createElement('table');
-	
-}
