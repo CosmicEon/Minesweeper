a4a004357437dee96129408d75c5b237c8465827
krisi0505 <krisi0505@abv.bg>
2017-05-09 14:05:05 +0300

    add tests

--- minisweepr/tests/tests.js
+++ minisweepr/tests/tests.js
@@ -7,7 +7,13 @@ import { generateBomb } from '../js/bombs.js';
 import {Board} from '../board.js';
 
 describe('Bombs tests', () => {
-	
+	it('generateBomb should return an array', function() {
+			var board = new Board(16, 16);
+            var actual = board.rows;
+            var expected = 16;
+
+            expect(actual).to.be.equal(expected);
+        });
 });
 
 describe('Board tests', () => {
@@ -22,6 +28,23 @@ describe('Board tests', () => {
 			var board = new Board(16, 16);
             var actual = board.cols;
             var expected = 16;
+
+            expect(actual).to.be.equal(expected);
+        });
+});
+
+describe('Timer tests', () => {
+	it('constructor should set correct number of minutes', function() {
+			var timer = new Timer();
+            var actual = timer.minutes;
+            var expected = 0;
+
+            expect(actual).to.be.equal(expected);
+        });
+	it('constructor should set correct number of seconds', function() {
+			var timer = new Timer();
+            var actual = timer.seconds;
+            var expected = 0;
 
             expect(actual).to.be.equal(expected);
         });
