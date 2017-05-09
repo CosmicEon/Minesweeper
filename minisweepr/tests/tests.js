3b72bf13b9b281c28ac7246c4a5b8a4bb89528bd
krisi0505 <krisi0505@abv.bg>
2017-05-09 13:44:49 +0300

    add board constructor tests

--- minisweepr/tests/tests.js
+++ minisweepr/tests/tests.js
@@ -4,11 +4,25 @@ const chai = require('chai');
 // classes to tests
 //const { generateBomb } = require('../js/bombs');
 import { generateBomb } from '../js/bombs.js';
+import {Board} from '../board.js';
 
 describe('Bombs tests', () => {
-
+	
 });
 
 describe('Board tests', () => {
+	it('constructor should set correct number of rows', function() {
+			var board = new Board(16, 16);
+            var actual = board.rows;
+            var expected = 16;
 
+            expect(actual).to.be.equal(expected);
+        });
+	it('constructor should set correct number of columns', function() {
+			var board = new Board(16, 16);
+            var actual = board.cols;
+            var expected = 16;
+
+            expect(actual).to.be.equal(expected);
+        });
 })
